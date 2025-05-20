9072d7cb95ee6e0b1ac9f123a2385a4099b8427a
faizqrs <faisalqrs19@gmail.com>
2025-05-20 14:09:26 +0530

    created Global Message GET API

--- routes/messages.js
+++ routes/messages.js
@@ -38,6 +38,51 @@ routes.post('/global', async (req, res) =>{
 
 })
 
+// Read Global Message api
+routes.get('/globalmessage', async (req, res) => {
+  // in this way sender name is missing
+  // let messages = await GlobalMessage.find(); // read all message from database
+  // res.send(messages);
+  // in this way sender name is available
+  let messages = await GlobalMessage.aggregate([ // aggregate function to perform data finding task
+    {
+      $lookup: {   // it is used to lookup in database
+        from: 'users', // from which collection
+        localField:'from', // localfield name in collection
+        foreignField:'_id', // field name in foreign collection
+        as:'userObj' // finding these thing as userObj
+      }
+    }
+  ])
+  // it will send response like this
+  //  {
+  //       "_id": "682b312462bc8bce978b2df5",
+  //       "from": "68231fa0e9d3c1c245da7a14",
+  //       "body": "hii this is message api",
+  //       "date": "1747661092427",
+  //       "__v": 0,
+  //       "userObj": [
+  //           {
+  //               "_id": "68231fa0e9d3c1c245da7a14",
+  //               "name": "noraiz",
+  //               "userName": "noraiz02",
+  //               "password": "$2b$10$EHY/egGOL/h2NmTZtsDgveFdiNmAIVIN9UyljkcgilhCs6GPnVQFa",
+  //               "date": "1747132238328",
+  //               "__v": 0
+  //           }
+  //       ]
+  //   },
+
+  // so now we need to hide few things in response
+  .project({
+    'userObj.password':0, // hiding from userObj
+    'userObj.date':0,
+    'userObj.__v':0,
+    '__v':0           // hiding from response
+  })
+  res.send(messages);
+})
+
 
 
 
